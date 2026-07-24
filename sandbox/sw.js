1ca33f79b60ca63700dc54b785a3518da7d92ec8
Claude (Cowork) <dubbolbiutifu@gmail.com>
2026-07-24 18:30:53 +0200

    sandbox: fix tab bar hidden behind Android nav bar after reset/reload (100svh)

--- sandbox/sw.js
+++ sandbox/sw.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // Bump this on any shell-file change so old installs pick up the update.
-var CACHE_NAME = "dd-sandbox-shell-v9";
+var CACHE_NAME = "dd-sandbox-shell-v10";
 
 var SHELL_FILES = [
   "./",
